543b9a593a7f517729e9938b7512cf313f8a7845
SolDanil <44667435+SolDanil@users.noreply.github.com>
2019-03-21 13:07:11 +0600

    model

---
+++ Table_oven.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Table_oven extends Model
+{
+    //
+
+    protected $fillable=['day','zagon1','zagon2','zagon3','zagon4','all_plus','all_minus'];
+}
